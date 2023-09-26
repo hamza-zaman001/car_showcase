712bd246f21501e04d5f1452a6ea73a37753dcd9
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-09-27 00:32:53 +0500

    .

---
+++ components/CustomButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+
+const CustomButton = () => {
+  return (
+    <button
+        disabled={false}
+    >
+      
+    </button>
+  )
+}
+
+export default CustomButton
