18241a808026cdf8f16f8e330969719cffb65ae5
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-10-07 00:20:39 +0500

    .

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,12 @@
+import { SearchManufacturerProps } from '@/types'
+import React from 'react'
+
+const SeachManufacturer = ({ manufacturer, setManufacturer } : SearchManufacturerProps) => {
+  return (
+    <div>
+      SeachManufacturer
+    </div>
+  )
+}
+
+export default SeachManufacturer
