83e9adb9a9041cb04f3e55660f2125e65f9d26b3
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-10-07 00:01:46 +0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,14 @@ export default function Home() {
     <main className="overflow-hidden">
       <Hero />
       
-      <div className='mt-12 padding-x padding-y'></div>
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+          <p>Explore the cars you might like</p>
+        </div>
+      </div>
     </main>
   )
 }
